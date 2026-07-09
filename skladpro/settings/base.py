7f677730ac8becc76aceba9d940ebc4749981727
imron1007 <ergesevmahamadzan@gmail.com>
2026-07-10 01:25:12 +0300

    Update base.py

--- skladpro/settings/base.py
+++ skladpro/settings/base.py
@@ -140,6 +140,8 @@ MEDIA_ROOT = BASE_DIR / config('MEDIA_ROOT', default='media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
